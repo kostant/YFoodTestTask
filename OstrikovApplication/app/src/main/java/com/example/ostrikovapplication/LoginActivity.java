26eb5e2aee62cbfdcbcd8c0cd84e9e9cbc7acf33
JIibaa <101796270+jiibaaa@users.noreply.github.com>
2023-09-05 17:09:36 +0300

    Update LoginActivity.java

--- OstrikovApplication/app/src/main/java/com/example/ostrikovapplication/LoginActivity.java
+++ OstrikovApplication/app/src/main/java/com/example/ostrikovapplication/LoginActivity.java
@@ -3,9 +3,11 @@ package com.example.ostrikovapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.textfield.TextInputEditText;
 
 public class LoginActivity extends AppCompatActivity {
@@ -18,5 +20,12 @@ public class LoginActivity extends AppCompatActivity {
         EditText loginEditText = (EditText) findViewById(R.id.login);
         EditText passwwordEditText = (EditText) findViewById(R.id.password);
         Button loginButton = (Button) findViewById(R.id.logIn);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Error password", Snackbar.LENGTH_LONG).show();
+            }
+        });
     }
 }
