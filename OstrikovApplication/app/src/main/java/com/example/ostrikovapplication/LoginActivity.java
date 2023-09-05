579a7d72ebe45f4d695c446829a8e6b292fcbb97
JIibaa <101796270+jiibaaa@users.noreply.github.com>
2023-09-05 16:41:05 +0300

    appl

---
+++ OstrikovApplication/app/src/main/java/com/example/ostrikovapplication/LoginActivity.java
@@ -0,0 +1,20 @@
+package com.example.ostrikovapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        EditText loginEditText = (EditText) findViewById(R.id.login);
+        EditText passwwordEditText = (EditText) findViewById(R.id.password);
+        Button loginButton = (Button) findViewById(R.id.logIn);
+    }
+}
